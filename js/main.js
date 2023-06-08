ec56e56a9003b5e9d6ac059f63af28818a4e7740
mverzi <maggie.verzi@gmail.com>
2023-06-08 11:15:44 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 alert('working')
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const rapperName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://rap-names-api-wq4y.onrender.com/api/${rapperName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.birthName
+    }catch(error){
+        console.log(error)
+    }
+}
