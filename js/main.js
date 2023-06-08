47ac31aec373f5e97021b8f1a9b0e0698c6d4675
mverzi <maggie.verzi@gmail.com>
2023-06-08 11:24:03 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://rap-names-api-wq4y.onrender.com/api/${rapperName}`)
+        const response = await fetch(`https://rap-names-api-mqvv.onrender.com/api/${rapperName}`)
         const data = await response.json()
 
         console.log(data)
